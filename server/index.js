6c41584fef23efb53d94ee94987380ef1c80bfb9
Ryan McCombs <mccombs008@yahoo.com>
2018-09-28 10:45:29 -0600

    security check

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
+
 const express = require('express');
 const controller = require('./controller');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,7 +11,7 @@ app.use(bodyParser.json());
 
 
 
-const port = 4000;  
+const port = process.env.PORT;  
 app.listen(port, ()=>{
     console.log(`Port ${port} is listening. Tell us all about it.`)
 })
